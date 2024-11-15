cdf89012ec404af0b3f87efac844257cab5243f0
Dima Yanovsky <yanovsky@mit.edu>
2024-11-15 13:26:41 -0500

    added transcribe video

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 # utils.py
+from openai import OpenAI
+from utils import update_metadata
+from moviepy.editor import VideoFileClip
 import os
 import json
 
@@ -9,14 +12,17 @@ metadata = {}
 
 VIDEO_DIR = "tiktok_data/videos/"
 
+
 def clean_url(url: str) -> str:
-   """Remove query parameters from URL."""
-   return url.split('?')[0]
+    """Remove query parameters from URL."""
+    return url.split('?')[0]
+
 
 def write_metadata():
     """Write metadata to disk. Slow, call sparingly."""
     with open('metadata.json', 'w') as f:
         f.write(json.dumps(metadata))
+
 
 def update_metadata(url: str, update_key: str, update_val):
     """Add field to a video's metadata."""
@@ -28,6 +34,7 @@ def update_metadata(url: str, update_key: str, update_val):
     if url not in metadata:
         metadata[url] = {}
     metadata[url][update_key] = update_val
+
 
 def download_video(url: str, csv_output_path: str):
     """Download TikTok video."""
@@ -44,6 +51,29 @@ def download_video(url: str, csv_output_path: str):
     new_video_path = os.path.join(VIDEO_DIR, video_filename)
     os.rename(video_path, new_video_path)
     update_metadata(url, "local_video_path", new_video_path)
+
+
+def transcribe_mp4(video_data):
+    video_path = video_data["local_video_path"]
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    video = VideoFileClip(video_path)
+    audio_filename = video_path.replace('.mp4', '.mp3')
+
+    video.audio.write_audiofile(audio_filename)
+    transcript = ""
+    with open(audio_filename, "rb") as audio_file:
+        transcript = client.audio.transcriptions.create(
+            file=audio_file,
+            model="whisper-1",
+        )
+
+    update_metadata(video_data["url"], "transcript",
+                    transcript["text"])
+
+    video.close()
+    os.remove(video_path)
+    os.remove(audio_filename)
+
 
 if __name__ == "__main__":
     download_video(
